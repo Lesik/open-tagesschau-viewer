a3bdaca4f471c17c2a281cddea348ee671db6054
Lesik <amborsdorfer@gmail.com>
2014-07-27 18:27:39 +0200

    ADD: FadingActionBar

--- src/org/androidfromfrankfurt/tagesschau/TagesschauActivity.java
+++ src/org/androidfromfrankfurt/tagesschau/TagesschauActivity.java
@@ -1,36 +1,77 @@
 package org.androidfromfrankfurt.tagesschau;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+import com.manuelpeinado.fadingactionbar.FadingActionBarHelper;
+
 import android.app.Activity;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 
 public class TagesschauActivity extends Activity {
+    private ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tagesschau);
+
+        FadingActionBarHelper helper = new FadingActionBarHelper()
+            .actionBarBackground(R.drawable.ab_background)
+            .headerLayout(R.layout.activity_tagesschau_header)
+            .contentLayout(R.layout.activity_tagesschau_content);
+        setContentView(helper.createView(this));
+        helper.initActionBar(this);
+
+        ListView listView = (ListView) findViewById(android.R.id.list);
+        ArrayList<String> items = loadItems(R.raw.sample_list);
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
+        listView.setAdapter(adapter);
     }
 
+    /**
+     * @return A list of Strings read from the specified resource
+     */
+    private ArrayList<String> loadItems(int rawResourceId) {
+        try {
+            ArrayList<String> countries = new ArrayList<String>();
+            InputStream inputStream = getResources().openRawResource(rawResourceId);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                countries.add(line);
+            }
+            reader.close();
+            return countries;
+        } catch (IOException e) {
+            return null;
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.tagesschau, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
+        if (item.getItemId() == R.id.action_settings) {
+            changeDataset();
             return true;
         }
         return super.onOptionsItemSelected(item);
     }
+
+    private void changeDataset() {
+      adapter.notifyDataSetChanged();
+  }
 }
